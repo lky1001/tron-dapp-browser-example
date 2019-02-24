eb78adec84c8e87b2c57d97303356ff5792529fc
kylee <kylee@devband.com>
2019-02-24 12:20:48 +0900

    layout

--- app/src/main/java/com/tistory/lky1001/trondappbrowser/MainActivity.java
+++ app/src/main/java/com/tistory/lky1001/trondappbrowser/MainActivity.java
@@ -2,12 +2,26 @@ package com.tistory.lky1001.trondappbrowser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.editText)
+    EditText privateKeyEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button)
+    public void onSaveClick() {
+        // todo - save preference
     }
 }
---
+++ app/src/main/java/com/tistory/lky1001/trondappbrowser/WebViewActivity.java
@@ -0,0 +1,20 @@
+package com.tistory.lky1001.trondappbrowser;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @BindView(R.id.webview)
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+        ButterKnife.bind(this);
+    }
+}
