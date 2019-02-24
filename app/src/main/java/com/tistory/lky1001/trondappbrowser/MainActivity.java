c49388109936b85807f3445760108e3ce221b7c6
kylee <kylee@devband.com>
2019-02-24 12:10:57 +0900

    init project

---
+++ app/src/main/java/com/tistory/lky1001/trondappbrowser/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tistory.lky1001.trondappbrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
