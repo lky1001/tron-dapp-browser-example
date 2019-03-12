81e81de3b9eec4f8ec0c40a7a6c2a13c14676e0a
lky1001 <lky1001@gmail.com>
2019-03-12 23:16:38 +0900

    android dapp browser test

--- test/src/App.js
+++ test/src/App.js
@@ -1,61 +1,97 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import TronWeb from 'tronweb'
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import TronWeb from "tronweb"
 
 // This provider is optional, you can just use a url for the nodes instead
 const HttpProvider = TronWeb.providers.HttpProvider
-const fullNode = new HttpProvider('https://api.trongrid.io') // Full node http endpoint
-const solidityNode = new HttpProvider('https://api.trongrid.io') // Solidity node http endpoint
-const eventServer = new HttpProvider('https://api.trongrid.io') // Contract events http endpoint
+const fullNode = new HttpProvider("https://api.trongrid.io") // Full node http endpoint
+const solidityNode = new HttpProvider("https://api.trongrid.io") // Solidity node http endpoint
+const eventServer = new HttpProvider("https://api.trongrid.io") // Contract events http endpoint
+
+const FOUNDATION_ADDRESS = "TWiWt5SEDzaEqS6kE5gandWMNfxR2B5xzg"
 
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      tronWeb : null,
-      balance : -1,
+      tronWeb: {
+        installed: false,
+        loggedIn: false
+      }
     }
   }
 
+  // test
+  // https://github.com/TronWatch/TronLink-Demo-Messages/blob/master/src/components/App/index.js
   async componentDidMount() {
-    let tronWeb;
-    let balance = -1;
+    await new Promise(resolve => {
+      const tronWebState = {
+        installed: !!window.tronWeb,
+        loggedIn: window.tronWeb && window.tronWeb.ready
+      }
 
-    if (window.tronWeb && window.tronWeb.requestData) {
-        tronWeb = new TronWeb(fullNode, solidityNode, eventServer, window.tronWeb.requestData())
+      if (tronWebState.installed) {
+        this.setState({
+          tronWeb: tronWebState
+        })
 
-        const dd = tronWeb.defaultAddress;
+        return resolve()
+      }
 
-        console.log(dd.base58)
+      let tries = 0
 
-        // The majority of the function calls are asynchronus,
-        // meaning that they cannot return the result instantly.
-        // These methods therefore return a promise, which you can await.
-        balance = await tronWeb.trx.getBalance(dd.base58)
-        console.log({ balance })
-    } else {
-        alert('no tronWeb')
-    }
+      const timer = setInterval(() => {
+        if (tries >= 10) {
+          const TRONGRID_API = "https://api.trongrid.io"
 
-    this.setState({
-        tronWeb: tronWeb,
-        balance: balance,
+          window.tronWeb = new TronWeb(TRONGRID_API, TRONGRID_API, TRONGRID_API)
+
+          this.setState({
+            tronWeb: {
+              installed: false,
+              loggedIn: false
+            }
+          })
+
+          clearInterval(timer)
+          return resolve()
+        }
+
+        tronWebState.installed = !!window.tronWeb
+        tronWebState.loggedIn = window.tronWeb && window.tronWeb.ready
+
+        if (!tronWebState.installed) return tries++
+
+        this.setState({
+          tronWeb: tronWebState
+        })
+
+        resolve()
+      }, 100)
     })
 
-    // // You can also bind a `then` and `catch` method.
-    // tronWeb.trx.getBalance(address).then(balance => {
-    //     console.log({balance});
-    // }).catch(err => console.error(err));
+    if (!this.state.tronWeb.loggedIn) {
+      // Set default address (foundation address) used for contract calls
+      // Directly overwrites the address object as TronLink disabled the
+      // function call
+      window.tronWeb.defaultAddress = {
+        hex: window.tronWeb.address.toHex(FOUNDATION_ADDRESS),
+        base58: FOUNDATION_ADDRESS
+      }
 
-    // // If you'd like to use a similar API to Web3, provide a callback function.
-    // tronWeb.trx.getBalance(address, (err, balance) => {
-    //     if (err)
-    //         return console.error(err);
+      window.tronWeb.on("addressChanged", () => {
+        if (this.state.tronWeb.loggedIn) return
 
-    //     console.log({balance});
-    // });
+        this.setState({
+          tronWeb: {
+            installed: true,
+            loggedIn: true
+          }
+        })
+      })
+    }
   }
 
   render() {
@@ -66,7 +102,12 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             balance : {this.state.balance}
           </a>
         </header>
